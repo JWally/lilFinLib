5329ab69dc6e0a70c15906883493ec528d996597
JustinWolcott <Justin.Wolcott@bbvacompass.com>
2015-04-08 16:49:57 -0500

    second commit

---
+++ gruntfile.js
@@ -0,0 +1,101 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        "jshint": {
+            "files": [
+                "src/**/*.js",
+                "!src/bower_components/**",
+                "!src/template/all.js",
+                "!src/app.build.js",
+                "!src/main.min.js",
+                "!src/modules/stripe.js",
+                "!src/modules/d3.js"
+            ],
+            "options": {
+                "quotmark": "double",
+                "camelcase": false,
+                "curly": true,
+                "eqeqeq": true,
+                "indent": 4,
+                "newcap": true,
+                "noempty": true,
+                "nonew": true,
+                "trailing": true,
+                "maxlen": 100,
+                "white": true
+            }
+        },
+        "jsbeautifier": {
+            "default": {
+                src: [
+                    "gruntfile.js",
+                    "src/**/*.js",
+                    "src/**/*.html",
+                    "src/**/*.scss",
+                    "!src/js/template/all.js",
+                    "!src/modules/d3.js",
+                    "!src/modules/stripe.js"
+
+                ]
+            },
+            "options": {
+                "html": {
+                    "brace_style": "collapse",
+                    "indent_char": " ",
+                    "indent_scripts": "keep",
+                    "indent_size": 4,
+                    "max_preserve_newlines": 10,
+                    "preserve_newlines": true,
+                    "unformatted": ["a", "sub", "sup", "b", "i",
+                        "u"
+                    ],
+                    "wrap_line_length": 0
+                },
+                "js": {
+                    "brace_style": "collapse",
+                    "break_chained_methods": false,
+                    "e4x": false,
+                    "eval_code": false,
+                    "indent_char": " ",
+                    "indent_level": 0,
+                    "indent_size": 4,
+                    "indent_with_tabs": false,
+                    "jslint_happy": true,
+                    "keep_array_indentation": false,
+                    "keep_function_indentation": false,
+                    "max_preserve_newlines": 10,
+                    "preserve_newlines": true,
+                    "space_before_conditional": true,
+                    "space_in_paren": false,
+                    "unescape_strings": false,
+                    "wrap_line_length": 90
+                },
+                "css": {
+                    "fileTypes": [".scss"],
+                    "indentChar": " ",
+                    "indentSize": 4
+                }
+            }
+        }
+
+
+
+
+
+    });
+
+    grunt.loadNpmTasks("grunt-jsbeautifier");
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.registerTask("prod", [
+        "jsbeautifier:default",
+        "jshint"
+    ]);
+
+    grunt.registerTask("dev", [
+        "jsbeautifier:default",
+        "jshint"
+    ]);
+
+
+
+
+}
---
+++ src/main.js
@@ -0,0 +1,142 @@
+var obj = {
+    pv: function (np, ir, pmt, fv, pb) {
+        var pv;
+        if (ir === 0.0) {
+            pv = -fv - np * pmt;
+        } else {
+            var qa = Math.pow(1 + ir, -np);
+            var qb = Math.pow(1 + ir, np);
+            pv = -qa * (fv + ((-1 + qb) * pmt * (1 + ir * pb)) / ir);
+        }
+        return pv;
+    },
+    fv: function (np, ir, pmt, pv, pb) {
+        var fv;
+        if (ir === 0.0) {
+            fv = -np * pmt - pv;
+        } else {
+            var q = Math.pow(1 + ir, np);
+            fv = -q * pv - (((-1 + q) * pmt * (1 + ir * pb)) / ir);
+        }
+        return fv;
+    },
+
+    pmt: function (np, ir, fv, pv, pb) {
+        var pmt = 0;
+        if (ir === 0.0) {
+            if (np !== 0.0) {
+                pmt = -(fv + pv) / np;
+            }
+        } else {
+            var q = Math.pow(1 + ir, np);
+            pmt = -(ir * (fv + (q * pv))) / ((-1 + q) * (1 + ir * pb));
+        }
+        return pmt;
+    },
+    nper: function (ir, pmt, fv, pv, pb) {
+        var np = 0;
+        if (ir === 0.0) {
+            if (pmt !== 0.0) {
+                np = -(fv + pv) / pmt;
+            }
+        } else { // ir !== 0
+            var terma = -fv * ir + pmt + ir * pmt * pb;
+            var termb = pmt + ir * pv + ir * pmt * pb;
+            np = Math.log(terma / termb) / Math.log(1 + ir);
+        }
+        return np;
+    },
+    // We're expecting the transactions to come
+    // in a 2d array like this:
+    // [
+    //  [duration, amount],
+    //  [duration, amount],
+    //  [duration, amount]
+    //]
+    npv: function (rate, transactions) {
+        var that = this,
+            total = 0;
+
+        //    pv: function (np, ir, pmt, fv, pb) {
+
+        for (var i = 0; i < transactions.length; i++) {
+            total += that.pv(
+                transactions[i][0],
+                rate,
+                0, -transactions[i][1],
+                1
+            );
+        }
+
+        return total;
+    },
+    // Solving Problems...Like a boss!
+    newton: function (guess, f, leftBound, rightBound, dx, minXdist, minYdist, exit) {
+        dx = dx || 1e-10;
+        minXdist = minXdist || 1e-10;
+        minYdist = minYdist || 1e-10;
+        exit = exit || 10;
+
+
+        //paste default: 1e-10, 1e-10, 1e-10, 10
+        var sequencePoints = [
+            [guess, f(guess)]
+        ];
+        var lineData, prevGuess, y;
+
+        var rslt,
+            deltay;
+
+        y = f(guess);
+        //newton loop
+        for (exit; exit > 0; exit -= 1) {
+            rslt = f(guess);
+            deltay = f(guess + dx) - rslt;
+            lineData = [guess, rslt, dx, deltay];
+            prevGuess = guess;
+            y = lineData[1];
+            guess = (-rslt * dx) / deltay + guess;
+            if ((guess < leftBound - dx) || (guess > rightBound + dx)) {
+                return [false, "Out of Bounds:" + guess, sequencePoints];
+            }
+            //add (a,f(a))
+            sequencePoints.push([guess, f(guess)]);
+            if ((Math.abs(guess - prevGuess) < minXdist) && (Math.abs(y) <
+                    minYdist)) {
+                return [true, guess, sequencePoints];
+            }
+        }
+        //did not meet the required distances
+        return [false, "Did not converge", sequencePoints];
+    },
+    // Welp...Here's our IRR Function
+    irr: function (transactions) {
+        var that = this;
+
+        return that.newton(0,
+            function(x){return that.npv(x, transactions)},
+            -100,
+            100
+        );
+    }
+};
+
+
+
+//We want this functions zeros
+var rslts = obj.irr([
+    [0, -1000],
+    [1, 100],
+    [2, 100],
+    [3, 100],
+    [4, 100],
+    [5, 100],
+    [6, 100],
+    [7, 100],
+    [8, 1000],
+    [8, 100]
+]);
+
+
+
+console.log(rslts);
